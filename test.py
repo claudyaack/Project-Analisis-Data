d9b03f551246b3b4f54ecae8503caab621db2435
claudyaack <audycindri@gmail.com>
2024-10-03 11:51:36 +0700

    Update test.py

--- test.py
+++ test.py
@@ -66,7 +66,7 @@ with tab2:
     
 
 with tab3:
-    st.subheader("Tren Rata-rata Penyewaan Sepeda per Jam pada Hari Kerja dan Hari Libur/Weekend")
+    st.subheader("Tren Rata-rata Penyewaan Sepeda per Jam pada Hari Kerja dan Hari Libur/Weekend (2011-2012)")
     # Memisahkan data berdasarkan hari kerja
     hari_kerja_df = hour_df.query('workingday == 1')
     avg_hari_kerja_df = hari_kerja_df.groupby('hr').agg({"cnt": "mean"})
@@ -79,15 +79,17 @@ with tab3:
     plt.figure(figsize=(12, 5))
     plt.plot(avg_hari_kerja_df.index, avg_hari_kerja_df['cnt'], label='Hari Kerja', color='blue')
     plt.plot(avg_hari_libur_df.index, avg_hari_libur_df['cnt'], label='Hari Libur/Weekend', color='red')
-    plt.title('Perbandingan Rata-Rata Banyak Penyewaan Sepeda per Jam', size=20)
+    plt.title('Perbandingan Rata-Rata Banyak Penyewaan Sepeda per Jam (2011-2012)', size=20)
     plt.xlabel('Jam',size=12)
     plt.ylabel('Banyak penyewaan',size=12)
     plt.legend()
     plt.xticks(range(24))
-    plt.grid(True)
+    plt.grid(True)   
+
     st.pyplot(plt)
+
   
-    st.subheader("Perbandingan Rata-rata Penyewaan per Jam antara Hari Kerja dan Hari Libur/Weekend")
+    st.subheader("Perbandingan Rata-rata Penyewaan per Jam antara Hari Kerja dan Hari Libur/Weekend (2011-2012)")
     # Membuat dropdown (selectbox) untuk memilih jam tertentu
     selected_hour = st.selectbox(
        'Pilih Jam:',
@@ -104,7 +106,7 @@ with tab3:
     plt.figure(figsize=(12, 5))
     plt.bar(['Hari Kerja', 'Hari Libur/Weekend'], [avg_kerja_selected['cnt'], avg_libur_selected['cnt']], color=['blue', 'red'])
 
-    plt.title(f'Perbandingan Rata-Rata Banyak Penyewaan Sepeda pada Jam {selected_hour}', size=20)
+    plt.title(f'Perbandingan Rata-Rata Banyak Penyewaan Sepeda pada Jam {selected_hour} (2011-2012)', size=20)
     plt.ylabel('Banyak Penyewaan', size=12)
     plt.grid(True, axis='y')
     
@@ -112,7 +114,7 @@ with tab3:
     st.pyplot(plt)
 
 with tab4 :
-    st.subheader("Tren Rata-rata Penyewaan Per Jam Berdasarkan Kategori Musim")
+    st.subheader("Tren Rata-rata Penyewaan Per Jam Berdasarkan Kategori Musim (2011-2012)")
     # Membuat dropdown (selectbox) untuk memilih kategori musim
     selected_season = st.selectbox(
         'Pilih Musim:',
@@ -130,13 +132,13 @@ with tab4 :
     sns.lineplot(x=avg_by_hour.index, y=avg_by_hour.values, marker='o')
     plt.xlabel('Jam')
     plt.ylabel('Rata-rata Banyak Penyewaan per Jam')
-    plt.title(f'Rata-rata Banyak Penyewaan Sepeda per Jam pada {selected_season}')
+    plt.title(f'Rata-rata Banyak Penyewaan Sepeda per Jam pada {selected_season} (2011-2012)')
     plt.xticks(range(24))
 
     # Tampilkan plot di Streamlit
     st.pyplot(plt)
 
-    st.subheader("Perbandingan Rata-rata Penyewaan per Jam Berdasarkan Kategori Musim")
+    st.subheader("Perbandingan Rata-rata Penyewaan per Jam Berdasarkan Kategori Musim (2011-2012)")
     # Membuat kolom 'season_category' untuk kategori musim
     hour_df['season_category'] = pd.cut(hour_df['season'], bins=[0, 1, 2, 3, 4], labels=['Musim Semi', 'Musim Panas', 'Musim Gugur', 'Musim Dingin'])
 
@@ -154,16 +156,16 @@ with tab4 :
     sns.barplot(x=avg_by_season.index, y=avg_by_season.values)
     plt.xlabel('Musim')
     plt.ylabel('Rata-rata Banyak Penyewaan per Jam')
-    plt.title(f'Rata-rata Banyak Penyewaan Sepeda pada Jam {selected_hour2} Berdasarkan Kategori Musim')
+    plt.title(f'Rata-rata Banyak Penyewaan Sepeda pada Jam {selected_hour2} Berdasarkan Kategori Musim (2011-2012)')
     
     # Tambahkan label di atas setiap bar
     for i in range(len(avg_by_season)):
-        plt.text(i, avg_by_season.values[i] -1, round(avg_by_season.values[i], 2), ha='center',va="center" )
+        plt.text(i, avg_by_season.values[i] -1, round(avg_by_season.values[i], 0), ha='center',va="center" )
 
     # Tampilkan plot di Streamlit
     st.pyplot(plt)
 
-    st.subheader("Tren Rata-rata Penyewaan per Jam Berdasarkan Kategori Cuaca")
+    st.subheader("Tren Rata-rata Penyewaan per Jam Berdasarkan Kategori Cuaca (2011-2012)")
     # Cerah/Sedikit Berawan', 'Berkabut/Berawan', 'Salju Ringan/Hujan Ringan', 'Hujan Berat/Salju Tebal'
     # Membuat dropdown (selectbox) untuk memilih kategori musim
     selected_weather = st.selectbox(
@@ -182,13 +184,13 @@ with tab4 :
     sns.lineplot(x=avg_by_hour.index, y=avg_by_hour.values, marker='o')
     plt.xlabel('Jam')
     plt.ylabel('Rata-rata Banyak Penyewaan per Jam')
-    plt.title(f'Rata-rata Banyak Penyewaan Sepeda per Jam pada Cuaca {selected_weather}')
+    plt.title(f'Rata-rata Banyak Penyewaan Sepeda per Jam pada Cuaca {selected_weather} (2011-2012)')
     plt.xticks(range(24))
 
     # Tampilkan plot di Streamlit
     st.pyplot(plt)
 
-    st.subheader("Perbandingan Rata-rata Penyewaan per Jam Berdasarkan Kategori Cuaca")
+    st.subheader("Perbandingan Rata-rata Penyewaan per Jam Berdasarkan Kategori Cuaca (2011-2012)")
     # Membuat kolom 'season_category' untuk kategori musim
     hour_df['season_category'] = pd.cut(hour_df['season'], bins=[0, 1, 2, 3, 4], labels=['Cerah/Sedikit Berawan', 'Berkabut/Berawan', 'Salju Ringan/Hujan Ringan', 'Hujan Berat/Salju Tebal'])
 
@@ -206,11 +208,11 @@ with tab4 :
     sns.barplot(x=avg_by_weather.index, y=avg_by_weather.values)
     plt.xlabel('Musim')
     plt.ylabel('Rata-rata Banyak Penyewaan per Jam')
-    plt.title(f'Rata-rata Banyak Penyewaan Sepeda pada Jam {selected_hour3} Berdasarkan Kategori Cuaca')
+    plt.title(f'Rata-rata Banyak Penyewaan Sepeda pada Jam {selected_hour3} Berdasarkan Kategori Cuaca (2011-2012)')
     
     # Tambahkan label di atas setiap bar
     for i in range(len(avg_by_weather)):
-        plt.text(i, avg_by_weather.values[i] - 1, round(avg_by_weather.values[i], 2), ha='center', va='center')
+        plt.text(i, avg_by_weather.values[i] - 1, round(avg_by_weather.values[i], 0), ha='center', va='center')
 
     # Tampilkan plot di Streamlit
     st.pyplot(plt)
